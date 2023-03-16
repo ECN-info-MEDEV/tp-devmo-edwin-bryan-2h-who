f1ee5e05035dab0737b2f3d3a19fd333ed2e3353
BryanH01 <92093032+BryanH01@users.noreply.github.com>
2023-03-16 08:40:19 +0100

    Initial commit

---
+++ app/src/main/java/com/example/who/data/LoginDataSource.java
@@ -0,0 +1,38 @@
+package com.example.who.data;
+
+import android.util.Log;
+
+import com.example.who.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            Log.d("login","in try");
+            if (username.equals("admin") && password.equals("admin")){
+                Log.d("login","before creating admin");
+                LoggedInUser admin =
+                        new LoggedInUser(
+                                java.util.UUID.randomUUID().toString(),
+                                "Admin");
+                Log.d("login","after creating admin");
+                return new Result.Success<>(admin);
+            } else {
+                return new Result.Error(new Exception("Failed to log in"));
+            }
+
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
